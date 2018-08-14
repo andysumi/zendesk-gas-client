3d9cc815edcdf186ce0950fad37f5fb792ef9fa5
andysumi <andysumi@ruby.net.tokai-u.jp>
2018-08-14 18:05:38 +0900

    レスポンスにstatusを追加

--- src/class.js
+++ src/class.js
@@ -3,7 +3,9 @@
 
     function ZendeskClient(subdomain, token) {
       this.apiUrl = 'https://' + subdomain + '.zendesk.com/api/v2';
-      this.headers = {'Authorization': 'Bearer ' + token};
+      this.headers = {
+        Authorization : 'Bearer ' + token
+      };
 
       if (!subdomain) throw new Error('"subdomain"は必須です');
       if (!token) throw new Error('"token"は必須です');
@@ -20,19 +22,17 @@
     ZendeskClient.prototype.fetch_ = function(resource, options) {
       var endPoint = this.apiUrl + resource;
       var response = UrlFetchApp.fetch(endPoint, {
-        'method': options.method,
-        'muteHttpExceptions': true,
-        'contentType': 'application/json; charset=utf-8',
-        'headers': this.headers,
-        'payload': options.payload || {}
+        method             : options.method,
+        muteHttpExceptions : true,
+        contentType        : 'application/json; charset=utf-8',
+        headers            : this.headers,
+        payload            : options.payload || {}
       });
 
-      if (response.getResponseCode() == 200) {
-        return JSON.parse(response.getContentText());
-      }
-
-      console.warn('Request failed. Expected 200, got %d: %s', response.getResponseCode(), response.getContentText());
-      return false;
+      return {
+        status : response.getResponseCode(),
+        body   : response.getContentText()
+      };
     };
 
     return ZendeskClient;
